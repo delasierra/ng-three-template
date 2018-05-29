f8cbe457a0e5491b4c3a38d73aca3c54dfea78ca
delasierra <carlos@csierrainteractive.com>
2018-05-29 10:31:48 +0100

    adding scroll-action2.service.ts

---
+++ src/app/services/scroll-action2.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+// import _ = require( "../../../node_modules/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js" );
+// import { ScrollMagic } from '../../../node_modules/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js';
+// import {ScrollMagic} from './scroll-action.service';
+@Injectable({
+    providedIn: 'root'
+})
+export class ScrollAction2Service {
+
+    constructor() {
+        // let controller = new ScrollMagic.Controller();
+
+        // let ScrollMagic = require("scrollmagic");
+        // require("../../../node_modules/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js");
+        // let controller = new ScrollMagic.Controller();
+    }
+}
